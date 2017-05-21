ad197d91c76dd4778f3051a1dfb68e4bb7681900
Thomas Junk <tjunk@intevation.de>
2017-05-21 12:36:10 +0200

    font name spelling corrected

--- config.def.h
+++ config.def.h
@@ -9,7 +9,7 @@ static const unsigned int snap      = 16;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Cousine:size=10" };
-static const char dmenufont[]       = "Cousin:size=10";
+static const char dmenufont[]       = "Cousine:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
